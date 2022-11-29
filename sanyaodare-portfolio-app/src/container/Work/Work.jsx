4ec804cab8b7c2fc91475bc30160ba92ab2b72c7
SanyaOdare <sanyaodare@gmail.com>
2022-11-29 14:21:57 +0100

    populate and style work component

--- sanyaodare-portfolio-app/src/container/Work/Work.jsx
+++ sanyaodare-portfolio-app/src/container/Work/Work.jsx
@@ -7,9 +7,77 @@ import { urlFor, client } from '../../client'
 import './Work.scss';
 
 const Work = () => {
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+
+    client.fetch(query)
+      .then((data) => {
+        setWorks(data);
+        setFilterWork(data);
+      })
+  }, [])
+  
+
+  const handleWorkFilter = (item) => {
+
+  }
+
   return (
-    <div>Work</div>
+    <>
+      <h2 className='head-text'>My Creative <span>Portfolio</span> section</h2>
+
+      <div className='app__work-filter'>
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__work-portfolio'
+      >
+        {filterWork.map((work, index) => (
+          <div 
+            className='app__work-item app__flex' key={index}>
+            <div className='app__work-img app__flex'>
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+
+              <motion.div
+                whileHover={{opacity: [0, 1]}}
+                transition={{ duration: 0.5, ease: 'easeInOut', staggerChildren: 0.5 }}
+                className='app__work-hover app__flex'
+              >
+                <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                  <motion.div
+                    whileInView={{scale: [0, 1]}}
+                    whileHover={{scale: [1, 0.9]}}
+                    transition={{ duration: 0.25}}
+                    className='app__flex'
+                  >
+                    <AiFillEye />
+                  </motion.div>
+
+                </a>
+
+              </motion.div>
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </>
   )
 }
 
-export default Work
+export default Work;
